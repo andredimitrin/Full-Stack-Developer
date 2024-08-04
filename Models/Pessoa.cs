c31d165e88827a0417373d63af1447b09be53a8f
André Dimitrin <82331544+andredimitrin@users.noreply.github.com>
2024-08-04 12:51:20 -0300

    att

---
+++ Models/Pessoa.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Full_Stack_Developer.Models
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; } = string.Empty;
+        public int Idade { get; set; }
+
+        public void Apresentar()
+        {
+            // Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos");
+            Console.WriteLine($"Olá, meu nome é {Nome}\nE tenho {Idade} anos");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using Full_Stack_Developer.Models;
+
+Pessoa pessoa1 = new()
+{
+    Nome = "André",
+    Idade = 37
+};
+pessoa1.Apresentar();
+
